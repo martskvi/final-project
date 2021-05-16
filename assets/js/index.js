d475f83ffc6cbedc883bf244af4e3829ece39f5e
martskvi <giorgi.martskvishvili@gmail.com>
2021-05-17 01:20:09 +0400

    add menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,11 @@ window.addEventListener("scroll", function(){
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0 );
 })
+
+const menu = document.querySelector('.menu');
+const menuOpen = () => {
+    menu.classList.add('visible');
+}
+const menuClose = () => {
+    menu.classList.remove('visible');
+}
